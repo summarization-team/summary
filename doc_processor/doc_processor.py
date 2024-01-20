9a72c5aefc590d676cfca44b999a837ee99e3dba
avecht <alyssavecht@gmail.com>
2024-01-20 10:32:56 -0800

    Updated get_AQUAINT_file, get_AQUAINT2_file, and get_year to correct missing characters from filepath

--- doc_processor/doc_processor.py
+++ doc_processor/doc_processor.py
@@ -5,19 +5,7 @@ class DocumentProcessor:
     def __init__(self, input_xml_file, output_dir):
         self.input_xml_file = input_xml_file
         self.output_dir = output_dir
-
-    # Return year from doc ID
-    def get_year(self, docID):
-        return int(docID[-13:])
-    
-    # Return filepath of AQUAINT xml document
-    def get_AQUAINT_file(docID):
-        filePath = '/corpora/LDC/LDC02T31/'
-        filePath += docID[:2] + '/'
-        filePath += get_year(docID) + '/'
-        filePath += docID[-13:-6] + '_' + docID[:2] + '_ENG'
-        return filePath
-
+        
     def process_documents(self):
         # Implement XML parsing and document processing
         # Return a list of DocSets
@@ -32,11 +20,36 @@ class DocumentProcessor:
             
             for doc in docset:
                 docID = doc.get('id')
-                if get_year(docID) >= 1996 and get_year(docID) <= 2000:
+                if int(get_year(docID)) >= 1996 and int(get_year(docID)) <= 2000:
                     file = get_AQUAINT_file(docID)
-                elif get_year(docID) >= 2004 and get_year(docID) <= 2006:
+                elif int(get_year(docID)) >= 2004 and int(get_year(docID)) <= 2006:
                     file = get_AQUAINT2_file(docID)
-                elif get_year(docID) == 2009:
+                elif int(get_year(docID)) == 2009:
                     file = get_2009_file(docID)
                              
         return docsets
+    
+# Return year from doc ID
+def get_year(docID):
+    return docID[-13:-9]
+    
+# Return filepath of AQUAINT xml document
+def get_AQUAINT_file(docID):
+    filePath = '/corpora/LDC/LDC02T31/'
+    filePath += docID[:3] + '/'
+    filePath += get_year(docID) + '/'
+    filePath += docID[-13:-5] + '_' + docID[:3] + '_ENG'
+    return filePath
+    
+# Return filepath of AQUAINT 2 xml document
+def get_AQUAINT2_file(docID):
+    filePath = '/corpora/LDC/LDC08T25/data/'
+    filePath += docID[:7].lower() + '/'
+    filePath += docID[:7].lower() + '_' + docID[-13:-7] + '.xml'
+    return filePath
+
+# Return filepath of 2009 xml document
+def get_2009_file(docID):
+    filePath = '~/dropbox/23-24/575x/TAC_2010_KBP_Source_Data/data/2009/nw'
+    # TODO figure out where any 2009 documents are and add logic
+    return filePath
