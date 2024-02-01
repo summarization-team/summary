e6f2abacaa7cc0254e66a3719b79fc4e070796d9
Karl <kharalds@uw.edu>
2024-02-01 18:00:23 -0500

    CHG run.sh path to main

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@ if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
     source "$HOME/anaconda3/etc/profile.d/conda.sh"
     conda activate Summary
 fi
-python3  ../src/main.py
+python3  ./src/main.py
