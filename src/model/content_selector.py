14722bbcce824f15826bc76de29b0ee4ace142c1
Ben C <bencote2000@gmail.com>
2024-02-13 15:19:42 -0800

    too long still and GloVe won't converge

--- src/model/content_selector.py
+++ src/model/content_selector.py
@@ -1,3 +1,4 @@
+import os
 import math
 import networkx as nx
 import nltk
@@ -115,40 +116,18 @@ class ContentSelector:
         return top_sentences
         
 
+    def select_content_textrank(self, all_documents, embeddings_path):
 
 
+        word_embeddings = {}
+        with open(embeddings_path, 'r') as f:
 
-    def get_sentence_embeddings(self, sentlist, tokenizer, model):
-        embeddings = []
-        for sentence in sentlist:
-            tokens = tokenizer.tokenize(sentence)
-            # add a classification token to help obtain fixed-size representation of sentence
-            # add a separator token to separate sentences
-            tokens = ['[CLS]'] + tokens + ['[SEP]']
-            id_vector = tokenizer.convert_tokens_to_ids(tokens)
-            # truncate or pad to make sure vector fits within BERT's sequence length limit
-            id_vector = id_vector[:512] + [0] * (512 - len(id_vector))
-
-            # get embedding from BERT model
-            with torch.no_grad():
-                outputvecs = model(torch.tensor([id_vector]))[0]
-            
-            # average embeddings for the tokens, exclude the CLS and SEP special tokens on either end
-            sent_embed = torch.mean(outputvecs[:,1:-1], dim=1).squeeze().numpy()
-            embeddings.append(sent_embed)
+            for line in f:
+                values = line.split()
+                word = values[0]
+                coefs = np.asarray(values[1:], dtype='float32')
+                word_embeddings[word] = coefs
         
-        return embeddings
-    
-
-
-    def select_content_textrank(self, all_documents):
-
-        # Load pre-trained model tokenizer
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        # Load pre-trained model
-        model = BertModel.from_pretrained('bert-base-uncased')
-        model.config
-        model.eval()  # Set the model to evaluation mode
 
         selected_sentences = {}
 
@@ -160,11 +139,18 @@ class ContentSelector:
                     sentlist_tok.append(sentence)
                     sentence_str = " ".join(sentence)
                     sentlist.append(sentence_str)
-            
-            embeddings = self.get_sentence_embeddings(sentlist, tokenizer, model)
 
-            embeddings = np.array(embeddings)
-            similarity_matrix = cosine_similarity(embeddings)
+            # create vectors for each sentence using CloVe embeddings
+            sentence_vectors = []
+            for i in sentlist_tok:
+                if len(i) != 0:
+                    sent_vec = sum([word_embeddings.get(w, np.zeros((50,))) for w in i])/(len(i)+0.001)
+                else:
+                    sent_vec = np.zeros((50,))
+                sentence_vectors.append(sent_vec)
+
+            # use cosine similarity to compare sentence vectors
+            similarity_matrix = cosine_similarity(sentence_vectors)
 
             # graph the resulting similarity matrix, then use the TextRank algorithm (thru PageRank) to find top sentence scores
             graph = nx.from_numpy_array(similarity_matrix)
@@ -181,6 +167,73 @@ class ContentSelector:
 
         # compiled dictionary of the top n sentences for each document
         return selected_sentences
+
+
+    # def get_sentence_embeddings(self, sentlist, tokenizer, model):
+    #     embeddings = []
+    #     for sentence in sentlist:
+    #         tokens = tokenizer.tokenize(sentence)
+    #         # add a classification token to help obtain fixed-size representation of sentence
+    #         # add a separator token to separate sentences
+    #         tokens = ['[CLS]'] + tokens + ['[SEP]']
+    #         id_vector = tokenizer.convert_tokens_to_ids(tokens)
+    #         # truncate or pad to make sure vector fits within BERT's sequence length limit
+    #         id_vector = id_vector[:100] + [0] * (100 - len(id_vector))
+
+    #         attention_mask = [1] * len(tokens) + [0] * (100 - len(tokens))
+    #         # get embedding from BERT model
+    #         with torch.no_grad():
+    #             outputvecs = model(torch.tensor([id_vector]), attention_mask=torch.tensor([attention_mask]))[0]
+    #         # average embeddings for the tokens, exclude the CLS and SEP special tokens on either end
+    #         sent_embed = torch.mean(outputvecs[:,1:-1], dim=1).squeeze().numpy()
+    #         embeddings.append(sent_embed)
+        
+    #     return embeddings
+    
+
+
+    # def select_content_textrank(self, all_documents):
+
+    #     # Load pre-trained model tokenizer
+    #     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    #     # Load pre-trained model
+    #     model = BertModel.from_pretrained('bert-base-uncased')
+    #     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #     model.to(device)
+    #     model.eval()  # Set the model to evaluation mode
+
+    #     selected_sentences = {}
+
+    #     for doc in all_documents.keys():
+    #         sentlist = []
+    #         sentlist_tok = []
+    #         for paragraph in all_documents[doc].keys():
+    #             for sentence in all_documents[doc][paragraph]:
+    #                 sentlist_tok.append(sentence)
+    #                 sentence_str = " ".join(sentence)
+    #                 sentlist.append(sentence_str)
+            
+    #         embeddings = self.get_sentence_embeddings(sentlist, tokenizer, model)
+    #         print("one document embeddings done")
+
+    #         embeddings = np.array(embeddings)
+    #         similarity_matrix = cosine_similarity(embeddings)
+
+    #         # graph the resulting similarity matrix, then use the TextRank algorithm (thru PageRank) to find top sentence scores
+    #         graph = nx.from_numpy_array(similarity_matrix)
+    #         sentence_scores = nx.pagerank(graph, alpha = 0.85, max_iter = 100) 
+            
+    #         ranked_sentindices = sorted(range(len(sentence_scores)), key=lambda index: sentence_scores[index], reverse=True)
+    #         top_sentindices = ranked_sentindices[:self.num_sentences_per_doc]
+
+    #         top_sentences = [sentlist_tok[i] for i in top_sentindices]
+
+
+    #         # stores top sentences as value in dictionary associated with the doc name as its key
+    #         selected_sentences[doc] = top_sentences
+
+    #     # compiled dictionary of the top n sentences for each document
+    #     return selected_sentences
 
 
 
@@ -259,7 +312,9 @@ class ContentSelector:
         if self.approach == 'tfidf':
             return self.select_content_tfidf(all_documents)
         elif self.approach == 'textrank':
-            return self.select_content_textrank(all_documents)
+            root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))  # Go two levels up to the project root
+            data_dir = os.path.join(root_dir, 'data', 'GloVe_data')
+            embeddings_path = os.path.join(data_dir, 'glove.6B.50d.txt')
+            return self.select_content_textrank(all_documents, embeddings_path)
         else:
             raise ValueError(f"Unknown approach: {self.approach}")
-
