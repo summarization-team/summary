8a4d0404370ec3cd551438eabe30feb1e636a51f
kharaldsson <karl.haraldsson@gmail.com>
2024-02-22 07:31:28 -0800

    ADD addl params to compression test

--- tests/test_content_realizer.py
+++ tests/test_content_realizer.py
@@ -40,11 +40,19 @@ class TestContentRealization(unittest.TestCase):
     # Updated Sentence Compression Tests to use model_id from config
     def setUp(self):
         # Updated to include model_id in the configuration
-        self.compression_method = SentenceCompressionMethod(additional_parameters={'model_id': 't5-small'})
+        self.compression_method = SentenceCompressionMethod(
+            additional_parameters= {
+                'model_id': 't5-base',
+                "max_length": 30,
+                "min_length": 5,
+                "do_sample": False
+                                   }
+        )
 
     def test_compression_single_sentence(self):
-        content = {"sentences": [["This", "is", "a", "very", "long", "sentence", "that", "should", "be", "compressed", "to", "something", "shorter", "."]]}
+        content = {"sentences": [['HONG', 'KONG', 'December', '8', 'Xinhua', 'Hong', 'Kong', 'health', 'authorities', 'are', 'stepping', 'up', 'the', 'hunt', 'for', 'the', 'source', 'of', 'a', 'deadly', 'influenza', 'strain', 'previously', 'found', 'only', 'in', 'birds', 'which', 'has', 'caused', 'a', 'global', 'alert' , '.']]}
         compressed_content = self.compression_method.realize(content)
+        print(compressed_content)
         self.assertIsInstance(compressed_content, str)
         self.assertTrue(len(compressed_content) < len(' '.join(content["sentences"][0])))
         self.assertNotEqual(compressed_content, '')
